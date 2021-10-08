b36ac654a70da0aa5ae1e41cf2afbf659692a2af
Rick <joao@htrindade.com.br>
2021-10-08 14:27:06 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route } from "react-router-dom";
+import Modal from "react-modal";
 
-import { useState } from 'react';
-import Modal from 'react-modal'
+import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./hooks/useTransactions";
+import { Login } from "./pages/Login/index";
 
-import {GlobalStyle} from './styles/global'
-import {  TransactionsProvider } from './hooks/useTransactions';
-import {Login} from './pages/Login/index'
+import "./services/firebase";
+import { Home } from "./pages/home/home";
 
-import './services/firebase'
-import { Home } from './pages/home/home';
-
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 export function App() {
-
-
-
-
   return (
     <BrowserRouter>
-      <Route path="/login" exact component={Login}/>
+      <Route path="/login" exact component={Login} />
       <TransactionsProvider>
-      <Route path="/" exact component={Home}/>
-      <GlobalStyle/>
-    </TransactionsProvider>
+        <Route path="/" exact component={Home} />
+        <GlobalStyle />
+      </TransactionsProvider>
     </BrowserRouter>
   );
 }
